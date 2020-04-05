d6f1cb8819c2c1cb8eca247927a2c457351cbab9
Jonathangmit <G00387830@gmit.ie>
2020-04-05 15:51:27 +0100

    plot images and testing6 added

---
+++ testing6.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from scipy import stats
+col=['sepal_length','sepal_width','petal_length','petal_width','class']
+iris=pd.read_csv("iris.csv",names=col)
+print(iris)
+iris_setosa=iris.loc[iris["class"]=="Iris-setosa"]
+iris_virginica=iris.loc[iris["class"]=="Iris-virginica"]
+iris_versicolor=iris.loc[iris["class"]=="Iris-versicolor"]
+sns.FacetGrid(iris,hue="class",height=7).map(sns.distplot,"petal_length",bins=7,).add_legend()
+sns.FacetGrid(iris,hue="class",height=7).map(sns.distplot,"petal_width",bins=7).add_legend()
+sns.FacetGrid(iris,hue="class",height=7).map(sns.distplot,"sepal_length",bins=7).add_legend()
+sns.FacetGrid(iris,hue="class",height=7).map(sns.distplot,"sepal_width",bins=7).add_legend()
+sns.pairplot(iris,hue="class")
+plt.show()
